9d292b6a331eddd979140e0c19df712e61241a18
shell909090@debox <shell909090@gmail.com>
2012-10-18 22:42:55 +0800

    + 增加单元测试系统，覆盖部分功能，便于调试和冒烟 * 修正mgr中whitenets的错误

---
+++ test/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@date: 2012-10-18
+@author: shell.xu
+'''
+import os, urllib2, unittest
+
+proxy_handler = urllib2.ProxyHandler({
+        'http': 'http://localhost:8080/', 'https': 'http://localhost:8080/'})
+# proxy_auth_handler = urllib2.ProxyBasicAuthHandler()
+# proxy_auth_handler.add_password('realm', 'host', 'username', 'password')
+proxy_opener = urllib2.build_opener(proxy_handler)
+
+class ProxyTest(unittest.TestCase):
+    def test_ingfw_http(self):
+        proxy_opener.open('http://www.sina.com.cn/')
+    def test_ingfw_https(self):
+        proxy_opener.open('https://www.cmbchina.com/')
+    def test_gfw_http(self):
+        proxy_opener.open('http://www.cnn.com/')
+    def test_gfw_https(self):
+        proxy_opener.open('https://www.facebook.com')
+
+auth_handler = urllib2.HTTPBasicAuthHandler()
+auth_handler.add_password(
+    realm='managers', uri='http://127.0.0.1:8080/', user='admin', passwd='uniproxy')
+auth_opener = urllib2.build_opener(auth_handler)
+
+class ManagerAuth(unittest.TestCase):
+    def test_noauth(self):
+        with self.assertRaises(urllib2.HTTPError):
+            urllib2.urlopen('http://127.0.0.1:8080/')
+    def test_auth(self):
+        auth_opener.open('http://127.0.0.1:8080/')
+
+class ManagerTest(unittest.TestCase):
+    def test_stat(self):
+        auth_opener.open('http://127.0.0.1:8080/')
+    def test_dnsfake(self):
+        auth_opener.open('http://127.0.0.1:8080/dnsfake')
+    def test_whitenets(self):
+        auth_opener.open('http://127.0.0.1:8080/whitenets')
+    def test_blacknets(self):
+        auth_opener.open('http://127.0.0.1:8080/blacknets')
+
+def main():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTests(loader.loadTestsFromModule(__import__('main')))
+    unittest.TextTestRunner(verbosity = 2).run(suite)
+
+if __name__ == '__main__': main()
